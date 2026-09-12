6f0a524e1ebca7f2eb47d70917f4b1b34964d88b
topiptv1997-jpg <topiptv1997@gmail.com>
2026-09-12 13:00:30 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -625,8 +625,10 @@ function waUrl(
   const clean =
     normalizePhone(number);
 
-  const base =
-    `https://wa.me/${clean}`;
+  // const base =
+  //   `https://wa.me/${clean}`;
+    const base =
+    `https://web.whatsapp.com/send/?phone=${clean}&amp;text&amp;type=phone_number&amp;app_absent=0`;
 
   if (!message) {
     return base;
